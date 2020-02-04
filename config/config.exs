282f5fa9079abf9a890299d3a4f2f8e10e059c7d
Diego Ferreira <dhferreira.ibm@gmail.com>
2020-02-04 09:30:01 -0300

    Updates on config for deployment

--- config/config.exs
+++ config/config.exs
@@ -11,12 +11,17 @@ config :banking_api,
   ecto_repos: [BankingApi.Repo],
   generators: [binary_id: true]
 
-token = System.get_env("SECRET_KEY_BASE") || raise "Error on secret"
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    (config.exs) environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
 
 # Configures the endpoint
 config :banking_api, BankingApiWeb.Endpoint,
   url: [host: "localhost"],
-  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  secret_key_base: secret_key_base,
   render_errors: [view: BankingApiWeb.ErrorView, accepts: ~w(json)],
   pubsub: [name: BankingApi.PubSub, adapter: Phoenix.PubSub.PG2]
 
@@ -35,7 +40,7 @@ import_config "#{Mix.env()}.exs"
 # Configures Guardian
 config :banking_api, BankingApi.Auth.Guardian,
   issuer: "banking_api",
-  secret_key: System.get_env("SECRET_KEY_BASE"),
+  secret_key: secret_key_base,
   permissions: %{
     default: [:banking],
     admin: [:backoffice]
--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,9 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :banking_api, BankingApiWeb.Endpoint,
-  url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME") || "localhost", port: 80]
+  http: [port: {:system, "PORT"}],
+  url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME") || "localhost", port: 80],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
   #cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
